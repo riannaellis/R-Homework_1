17a6b0e750bb3ddfde0b74732af1a5481b84d44a
hargu <hargu@10.7.77.183>
2024-02-02 11:32:09 -0600

    added Joseph Hargus

--- src/hw1/main.java
+++ src/hw1/main.java
@@ -6,6 +6,9 @@ public class main {
 		
 		Homework_1 member1 = new Homework_1("Rianna Ellis", "@riannaellis");
 		System.out.println("Name: " + member1.getName() + ", UserId: " + member1.getUserID());
+		
+		Homework_1 member2 = new Homework_1("Joseph Hargus", "@JosephHargus");
+		System.out.println("Name: " + member2.getName() + ", UserId: " + member2.getUserID());
 
 	}
 
