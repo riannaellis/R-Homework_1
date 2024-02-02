9757818ba81260e4a6c8d28434176c8c7efe2fc9
riannaellis <riannaellis@colins-iphone-2.dhcp.missouri.edu>
2024-02-02 11:09:12 -0600

    Created the Homework_1 class to create members
    This class contains the constructor, getters, and setters for the
    Homework_1 class, which has name and userID properties so each member in
    our group can create an instance of themselves with their own name and
    GitHub userID.

---
+++ src/hw1/Homework_1.java
@@ -0,0 +1,28 @@
+package hw1;
+
+public class Homework_1 {
+	
+	private String name;
+	private String userID;
+	
+	public Homework_1(String name, String userID) {
+		this.name = name;
+		this.userID = userID;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getUserID() {
+		return userID;
+	}
+
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}	
+}
