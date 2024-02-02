fa273877d80e0c84cad251ad2ceaf8e88dde1f17
riannaellis <riannaellis@colins-iphone-2.dhcp.missouri.edu>
2024-02-02 11:07:05 -0600

    Added main Java class for printing
    This is the main Java class for us to create instances of the Homework_1
    class and print information. Right now, it just creates one member to
    hold my own information and then prints it.

---
+++ src/hw1/main.java
@@ -0,0 +1,12 @@
+package hw1;
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		Homework_1 member1 = new Homework_1("Rianna Ellis", "@riannaellis");
+		System.out.println("Name: " + member1.getName() + ", UserId: " + member1.getUserID());
+
+	}
+
+}
